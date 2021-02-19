b1e219fbc14ed0ec099f3ac7544bb49a30cb2c62
slayersup <55502595+slayersup@users.noreply.github.com>
2021-02-19 18:00:09 +0200

    cleared code

--- src/test/java/guru/qa/simpletests/SimpleTests.java
+++ src/test/java/guru/qa/simpletests/SimpleTests.java
@@ -1,5 +1,6 @@
 package guru.qa.simpletests;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
@@ -13,29 +14,22 @@ public class SimpleTests {
     void assertSimplePositiveTest() {
         result = 2 + 2;
 
-        step("Compare \"result\" with 4 via assertEquals", () -> {
-            assertEquals(4, result);
-        });
+        step("Compare \"result\" with 4 via assertEquals", () -> assertEquals(4, result));
         step("Add 1 to \"result\"", () -> {
             result++;
         });
-        step("Compare \"result\" with 5 via assertTrue", () -> {
-            assertTrue(result == 5);
-        });
+        step("Compare \"result\" with 5 via assertTrue", () -> assertTrue(result == 5));
         step("Add 10 to \"result\"", () -> {
             result += 10;
         });
-        step("Compare \"result\" with 10 via assertFalse", () -> {
-            assertFalse(result == 10);
-        });
+        step("Compare \"result\" with 10 via assertFalse", () -> assertFalse(result == 10));
     }
 
     @Test
+    @Disabled
     void assertSimpleNegativeTest() {
         result = 2 + 2;
 
-        step("Compare \"result\" with 5 via assertTrue", () -> {
-            assertTrue(result == 5);
-        });
+        step("Compare \"result\" with 5 via assertTrue", () -> assertTrue(result == 5));
     }
 }
