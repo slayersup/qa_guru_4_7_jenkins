6ce7d76d7a57599a7b247c79abca33c8ae3ab1d7
slayersup <55502595+slayersup@users.noreply.github.com>
2021-02-19 18:26:15 +0200

    added Hamcrest tests

--- src/test/java/guru/qa/simpletests/SimpleTests.java
+++ src/test/java/guru/qa/simpletests/SimpleTests.java
@@ -1,9 +1,10 @@
 package guru.qa.simpletests;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleTests {
@@ -11,9 +12,10 @@ public class SimpleTests {
     static int result;
 
     @Test
-    void assertSimplePositiveTest() {
-        result = 2 + 2;
-
+    void assertSimpleSuccessTest() {
+        step("Set \"result\" to 2 + 2", () -> {
+            result = 2 + 2;
+        });
         step("Compare \"result\" with 4 via assertEquals", () -> assertEquals(4, result));
         step("Add 1 to \"result\"", () -> {
             result++;
@@ -26,9 +28,26 @@ public class SimpleTests {
     }
 
     @Test
-    void assertSimpleNegativeTest() {
-        result = 2 + 2;
-
+    void assertSimpleFailedTest() {
+        step("Set \"result\" to 2 + 2", () -> {
+            result = 2 + 2;
+        });
         step("Compare \"result\" with 5 via assertTrue", () -> assertTrue(result == 5));
+    }
+
+    @Test
+    void assertSimpleSuccessWithHamcrestTest() {
+        step("Set \"result\" to 2 + 2", () -> {
+            result = 2 + 2;
+        });
+        step("Compare \"result\" with 4 via assertThat", () -> assertThat(result, equalTo(4)));
+    }
+
+    @Test
+    void assertSimpleFailedWithHamcrestTest() {
+        step("Set \"result\" to 2 + 2", () -> {
+            result = 2 + 2;
+        });
+        step("Compare \"result\" with 5 via assertTrue", () -> assertThat(result, equalTo(5)));
     }
 }
